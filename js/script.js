f9cd5117dfc6b43981a6d757f6dd58b47a7d6ebe
Zhi Ti Kuan <zhitikuan@gmail.com>
2026-09-05 23:39:27 +0800

    Fix: moving text cursor and spinner

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
+        if (top >= offset && top < offset + height) {
             // active navbar links
             navLinks.forEach(links => {
                 links.classList.remove('active');
